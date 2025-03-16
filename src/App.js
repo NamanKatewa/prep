9ce65c548d90f506b221274811dbbe437fc628b3
Naman Katewa <126333629+sehaj2715@users.noreply.github.com>
2025-03-17 02:14:32 +0530

    Cleaning Up & Initialization of rendering sample floor plan using d3.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { json } from "d3";
 
 function App() {
-  return <div className="App"></div>;
+  const [plan, setPlan] = useState(null);
+  useEffect(() => {
+    json("/floorPlan.json")
+      .then((data) => {
+        setPlan(data);
+      })
+      .catch((error) => {
+        console.error("Error loading floor plan:", error);
+      });
+  }, []);
+  console.log(plan);
+
+  return (
+    <div className="App">
+      {plan ? (
+        <svg width={plan.dimensions.width} height={plan.dimensions.height}>
+          <rect
+            width={plan.dimensions.width}
+            height={plan.dimensions.height}
+            fill="000000"
+          ></rect>
+        </svg>
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
 }
 
 export default App;
